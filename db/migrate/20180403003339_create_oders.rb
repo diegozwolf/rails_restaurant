cfab76da0e5224e3c20e67c3e52b8e447068cbff
Diego Zuluaga <diegozuluaga88@gmail.com>
2018-04-02 21:14:27 -0500

    create crud

---
+++ db/migrate/20180403003339_create_oders.rb
@@ -0,0 +1,10 @@
+class CreateOders < ActiveRecord::Migration[5.1]
+  def change
+    create_table :orders do |t|
+      t.string :waiter
+      t.integer :board
+
+      t.timestamps
+    end
+  end
+end
